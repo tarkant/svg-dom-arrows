bdf3ee9dfd244fb7b84011e15ea684cd02eb7871
Tarek Jellali <jellalitarek@gmail.com>
2021-03-27 22:10:36 +0100

    add: reusable function for easier testing

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,45 @@
 import './styles.scss';
 
-const start = document.querySelector('.start');
-const end = document.querySelector('.end');
+const s = document.querySelector('.start');
+const e = document.querySelector('.end');
 
-const startBbox = start.getBoundingClientRect();
-const endBbox = end.getBoundingClientRect();
+function drawLine(start: Element, end: Element, debug = false) {
+  const startBbox = start.getBoundingClientRect();
+  const endBbox = end.getBoundingClientRect();
 
-const svgpath = document.querySelector('#svgpath') as SVGElement;
-const svgpathline = document.querySelector('#svgpath path') as SVGPathElement;
+  const svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const svgPathLine = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  document.body.appendChild(svgPath);
 
-const width = Math.abs(endBbox.left - startBbox.left);
-const height = Math.abs(endBbox.top - startBbox.top);
+  svgPath.appendChild(svgPathLine);
 
-const offsetX = startBbox.left > endBbox.left ? startBbox.left - endBbox.left : 0;
-const offsetY = startBbox.top > endBbox.top ? startBbox.top - endBbox.top : 0;
+  const width = Math.abs(endBbox.left - startBbox.left);
+  const height = Math.abs(endBbox.top - startBbox.top);
 
-// M Y1,Y2 X1,X2  for a simple line
-svgpathline.setAttribute('d', `M ${0 + offsetX},${0 + offsetY} ${width - offsetX},${height - offsetY}`);
-// M Yp1,Yp2 C Xc1,Yc1 Xc2,Yc2 Xp1,Xp2
-svgpathline.setAttribute('d', `M ${0 + offsetX},${0 + offsetY} C 0,${height / 2} ${width / 2},${height} ${width - offsetX},${height - offsetY}`);
+  const offsetX = startBbox.left > endBbox.left ? startBbox.left - endBbox.left : 0;
+  const offsetY = startBbox.top > endBbox.top ? startBbox.top - endBbox.top : 0;
 
-svgpath.setAttribute('width', `${width}`);
-svgpath.setAttribute('height', `${height}`);
+  // M Y1,Y2 X1,X2  for a simple line
+  svgPathLine.setAttribute('d', `M ${0 + offsetX},${0 + offsetY} ${width - offsetX},${height - offsetY}`);
 
-svgpath.style.top = `${startBbox.top - offsetY}px`;
-svgpath.style.left = `${startBbox.left - offsetX}px`;
+  // M Yp1,Yp2 C Xc1,Yc1 Xc2,Yc2 Xp1,Xp2
+  // svgpathline.setAttribute('d', `M ${0 + offsetX},${0 + offsetY} C 0,${height / 2} ${width / 2},${height} ${width - offsetX},${height - offsetY}`);
 
-console.table(startBbox);
-console.table(endBbox);
+  svgPath.setAttribute('width', `${width}`);
+  svgPath.setAttribute('height', `${height}`);
+
+  if (debug) {
+    svgPath.style.position = 'absolute';
+    svgPath.style.background = 'rgba(128,0,0,.2)';
+    svgPath.style.overflow = 'visible';
+    svgPathLine.setAttribute('style', 'stroke:white;stroke-width:4;fill:transparent');
+  }
+
+  svgPath.style.top = `${startBbox.top - offsetY}px`;
+  svgPath.style.left = `${startBbox.left - offsetX}px`;
+
+  console.table(startBbox);
+  console.table(endBbox);
+}
+drawLine(s, e, true);
+
