058db6b5111390071543bcff48af4178eb222067
Tarek Jellali <jellalitarek@gmail.com>
2023-02-24 23:51:17 +0100

    add: sample test page with screenshots

---
+++ tests/example-page.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await expect(page).toHaveTitle(/DOM SVG Arrows test page/);
+});
+
+test('visually test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await expect(page).toHaveScreenshot();
+});
