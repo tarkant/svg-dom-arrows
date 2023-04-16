80e9bd24047fc73333c5062e22aa9319ef2d6d50
Tarek Jellali <jellalitarek@gmail.com>
2023-04-16 18:00:44 +0200

    fix: element positioning relative to the appendTo element

--- src/paths/LinePath.ts
+++ src/paths/LinePath.ts
@@ -238,9 +238,17 @@ export class LinePath {
     this.containerDiv.style.width = `${width}px`;
     this.containerDiv.style.height = `${height}px`;
 
-    // Take into account the window.page(X|Y)Offset to avoid weird scroll issues
-    this.containerDiv.style.top = `${top + window.pageYOffset}px`;
-    this.containerDiv.style.left = `${left + window.pageXOffset}px`;
+    // Take into account the appendTo element's offset for correct positioning
+    if (this.options.appendTo) {
+      const offsetYAppended = this.options.appendTo.getBoundingClientRect().y;
+      const offsetXAppended = this.options.appendTo.getBoundingClientRect().x;
+      this.containerDiv.style.top = `${top - offsetYAppended}px`;
+      this.containerDiv.style.left = `${left - offsetXAppended}px`;
+    } else {
+      // Take into account the window.page(X|Y)Offset to avoid weird scroll issues
+      this.containerDiv.style.top = `${top + window.pageYOffset}px`;
+      this.containerDiv.style.left = `${left + window.pageXOffset}px`;
+    }
   }
 
   /**
